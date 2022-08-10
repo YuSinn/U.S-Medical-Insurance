91b8612513a9108cac7964dfb4fe2ca61544f4ec
Vladut Gabriel Alexandru <alexandru.vladut.gabriel97@gmail.com>
2022-08-10 12:10:10 +0200

    Update to the python file

--- US Medical Insurance.py
+++ US Medical Insurance.py
@@ -1,3 +1,20 @@
 # Python project : "U.S Medical Insurance"
 # Vladut Alexandru Gabriel
 # Version 0.1
+
+import pandas as pd
+
+# creating the insurance.csv dataframe
+ins_df = pd.read_csv("insurance.csv")
+
+# listing first 5 rows
+print(ins_df.head())
+
+# creating list with every column from the dataframe
+ages = ins_df.age
+sexes = ins_df.sex
+bmis = ins_df.bmi
+num_children = ins_df.children
+smoker_statuses = ins_df.smoker
+regions = ins_df.region
+insurance_charges = ins_df.charges
