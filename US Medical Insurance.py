cb4ec0482c33b4c1c95fb70a9869aaab3f4f1ed1
Vladut Gabriel Alexandru <alexandru.vladut.gabriel97@gmail.com>
2022-08-11 11:09:31 +0200

    Update to python file

--- US Medical Insurance.py
+++ US Medical Insurance.py
@@ -3,12 +3,13 @@
 # Version 0.1
 
 import pandas as pd
+import numpy as np
 
 # creating the insurance.csv dataframe
 ins_df = pd.read_csv("insurance.csv")
 
 # listing first 5 rows
-print(ins_df.head())
+#print(ins_df.head())
 
 # creating list with every column from the dataframe
 ages = ins_df.age
@@ -18,3 +19,7 @@ num_children = ins_df.children
 smoker_statuses = ins_df.smoker
 regions = ins_df.region
 insurance_charges = ins_df.charges
+
+print(round(np.average(ages),0))
+       
+    
